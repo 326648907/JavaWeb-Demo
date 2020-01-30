a8ba31f95881786094e7592675a844fbd46a5ef0
liuliu <326648907@qq.com>
2020-01-30 12:23:12 +0800

    My first commit

---
+++ src/cn/itcast/web/servlet/LoginServlet.java
@@ -0,0 +1,67 @@
+package cn.itcast.web.servlet;
+
+import cn.itcast.dao.UserDao;
+import cn.itcast.domain.User;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("接收到客户端信息");
+        //1.设置编码
+        req.setCharacterEncoding("utf-8");
+       /* //2.获取请求参数
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        //3.封装user对象
+        User loginUser = new User();
+        loginUser.setUsername(username);
+        loginUser.setPassword(password);*/
+        //2.获取所有请求参数
+        Map<String, String[]> map = req.getParameterMap();
+        //3.创建User对象
+        User loginUser = new User();
+        //3.2使用BeanUtils封装
+        try {
+            BeanUtils.populate(loginUser,map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+
+        //4.调用UserDao的login方法
+        UserDao dao = new UserDao();
+        User user = dao.login(loginUser);
+
+        //5.判断user
+        if(user == null){
+            //登录失败
+            req.getRequestDispatcher("/failServlet").forward(req,resp);
+            System.out.println("登录失败");
+        }else{
+            //登录成功
+            //存储数据
+            req.setAttribute("user",user);
+            //转发
+            req.getRequestDispatcher("/successServlet").forward(req,resp);
+            System.out.println("登录成功");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
